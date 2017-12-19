69fd96608f8ddb026f13bbfec248d52cbe9a9a73
Biozid Bostami <biozid.bostami@welldev.io>
2017-12-19 10:11:11 +0600

    add cell number

--- spec/anonymizer/mutator_spec.rb
+++ spec/anonymizer/mutator_spec.rb
@@ -5,7 +5,7 @@ module FakePipe
     context 'happy path' do
       it 'lists mutators' do
         expect(described_class.list)
-          .to contain_exactly('address_city', 'address_country', 'address_line_1', 'address_line_2', 'address_postcode', 'address_state', 'bcrypt_password', 'bcrypt_salt', 'clean_phone_number', 'company_catch_phrase', 'company_name', 'email', 'empty_curly', 'empty_bracket', 'empty_string', 'first_name', 'full_name', 'last_name', 'lorem_paragraph', 'lorem_sentence', 'lorem_word', 'md5', 'phone_ext', 'phone_number', 'url', 'user_name', 'uuid', 'guid', 'ugcid', 'vat_number', 'address_zipcode', 'bank_name')
+          .to contain_exactly('address_city', 'cell_phone', 'address_country', 'address_line_1', 'address_line_2', 'address_postcode', 'address_state', 'bcrypt_password', 'bcrypt_salt', 'clean_phone_number', 'company_catch_phrase', 'company_name', 'email', 'empty_curly', 'empty_bracket', 'empty_string', 'first_name', 'full_name', 'last_name', 'lorem_paragraph', 'lorem_sentence', 'lorem_word', 'md5', 'phone_ext', 'phone_number', 'url', 'user_name', 'uuid', 'guid', 'ugcid', 'vat_number', 'address_zipcode', 'bank_name')
       end
 
       context '#mutate_clean_phone_number' do
@@ -195,6 +195,12 @@ module FakePipe
       context '#mutate_empty_curly' do
         it 'matches proper format' do
           expect(described_class.mutate('empty_curly', 'dont_care')).to eq '{}'
+        end
+      end
+
+      context '#mutate_empty_curly' do
+        it 'matches proper format' do
+          expect(described_class.mutate('cell_phone', 'dont_care')).to match /[0-9()]+/
         end
       end
 
